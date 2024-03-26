ac62bf7df31d72e914b8941b68a8fed3667d3300
KoVlr <kovlrfw@gmail.com>
2024-03-26 17:08:44 +0300

    Added publication of a series of values

--- device_simulation/device_simulation.py
+++ device_simulation/device_simulation.py
@@ -1,11 +1,14 @@
 from random import randint
+from time import sleep
 import paho.mqtt.client as mqtt
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.connect("127.0.0.1")
 
 mqttc.loop_start()
-msg_info = mqttc.publish("iot_gg", f"weather temperature={randint(20, 25)}")
-msg_info.wait_for_publish()
+for i in range(20):
+    msg_info = mqttc.publish("iot_gg", f"weather temperature={randint(20, 25)}")
+    msg_info.wait_for_publish()
+    sleep(1)
 mqttc.disconnect()
 mqttc.loop_stop()
