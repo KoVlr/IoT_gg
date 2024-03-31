0990a0f5d3a90f7c6f16de264c0f5debfd74099c
KoVlr <kovlrfw@gmail.com>
2024-03-31 21:31:23 +0300

    Regain publication of a series of values

--- device_simulation/main.py
+++ device_simulation/main.py
@@ -1,6 +1,11 @@
 import paho.mqtt.client as mqtt
+from time import sleep
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.connect("mosquitto")
-mqttc.publish("iot_gg", "weather temperature=20")
+
+for i in range(60):
+    mqttc.publish("iot_gg", f"weather temperature={i}")
+    sleep(10)
+
 mqttc.disconnect()
